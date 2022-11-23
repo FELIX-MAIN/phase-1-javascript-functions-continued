f84f8d247aa9391b8fd971a0654061d6dab14894
FELIX-MAIN <117909509+FELIX-MAIN@users.noreply.github.com>
2022-11-24 00:55:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+// code your solution here
+function saturdayFun(party = "roller-skate"){
+    console.log(`This saturday I want to ${party}`);
+}
+saturdayFun()
+
+function mondayWork(drive = "go to the office"){
+    console.log(`This Monday, I will ${drive}`)
+}
+mondayWork()
+
+function wrapAdjective(special = "*"){
+    console.log(`You are ${special}`)
+}
+wrapAdjective()
